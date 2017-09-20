d3cce31ea15488afa6f5fcfb65b0031317c59e0b
Christopher Foo <chris.foo@gmail.com>
2017-09-20 01:49:50 -0400

    cesor: Munge links instead of entirely hiding them

--- chatbot383/censor.py
+++ chatbot383/censor.py
@@ -1,4 +1,6 @@
 import re
+import urllib.parse
+
 
 NAUGHTY_WORDS = tuple('''
 anal
@@ -81,16 +83,53 @@ NAUGHTY_REGEX = re.compile(
     re.IGNORECASE
 )
 
+SUBSTRING_NAUGHTY_REGEX = re.compile(
+    '|'.join(
+        re.escape(word) for word in NAUGHTY_WORDS
+    ),
+    re.IGNORECASE
+)
+
+
+LINK_WHITELIST = frozenset([
+    '.twitch.tv',
+])
+
 
 def censor_text(text: str) -> str:
     return NAUGHTY_REGEX.sub('***', text)
 
 
+def is_link_whitelisted(link: str) -> bool:
+    if not link.startswith('http'):
+        link = 'http://{}'.format(link)
+
+    result = urllib.parse.urlparse(link)
+    hostname = result.hostname or ''  # type:str
+
+    if not hostname:
+        return False
+
+    for whitelist_part in LINK_WHITELIST:
+        if hostname.endswith(whitelist_part) \
+                or '.{}'.format(hostname) == whitelist_part:
+            return True
+
+    return False
+
+
 def censor_link(text: str) -> str:
     def _regex_callback(match):
-        return '{}<naughty link>'.format(
-            match.group(1),
-        )
+        link = match.group(2)
+
+        if is_link_whitelisted(link):
+            return '{}{}'.format(match.group(1), match.group(2))
+        else:
+            shibe = '<:PancakeShibe:349613344572833815>'
+            return '{}<naughty link {}>'.format(
+                match.group(1),
+                SUBSTRING_NAUGHTY_REGEX.sub(shibe, match.group(2), 2).replace('.', shibe, 2)
+            )
 
     return re.sub(r'(\s|^)((?:https?\S+)|(?:\S+\.[a-zA-Z]{1,10}\S*))', _regex_callback, text)
 
